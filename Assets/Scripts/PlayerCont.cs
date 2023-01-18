71743443d8e96ad47d92661df84ef3fc34a3932e
PureFurry <66850379+PureFurry@users.noreply.github.com>
2023-01-19 00:26:00 +0300

    Adding Character And Camera Script
    Have a issur from jumping. Must be solve

--- Assets/Scripts/PlayerCont.cs
+++ Assets/Scripts/PlayerCont.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerCont : MonoBehaviour
 {
-    // Start is called before the first frame update
+ 
+    public float player_speed;
+    public float horizontal;
+    public Rigidbody2D player_r2d;
+    public GameObject playerObject;
+    public Vector2 jumpHeight;
+    public bool isJumping;
     void Start()
     {
-        
+        player_r2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
+        GetInput();
+        Movement();
         
+    }
+    void GetInput(){
+        horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+        else isJumping = false;
+    }
+    void Movement(){
+        player_r2d.velocity = new Vector2(horizontal*player_speed, player_r2d.position.y);
+        if (isJumping == true)
+        {
+            player_r2d.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
     }
 }
